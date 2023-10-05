a06d2743f5920585455a59f020a165f3eaac8d64
Arkadiy2907 <arkadiy2907@gmail.com>
2023-10-05 22:23:45 +0300

    added style

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,54 @@
 import React, { useState } from "react";
+import styled from "@emotion/styled";
+
+const Wrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+
+  .MyWrap {
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+    flex-direction: column;
+    padding: 5px;
+    gap: 5px;
+    width: max-content;
+    font-family: sans-serif;
+
+    .sting {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      width: 100%;
+
+      .label {
+        margin: 0 auto;
+        font-size: 1rem;
+        padding-right: 0.2rem;
+      }
+    }
+
+    .btn {
+      padding: 0.3rem 0.8rem;
+      background: none;
+      color: green;
+      border: 1px solid green;
+      transition: all ease 0.8s;
+      cursor: pointer;
+      border-radius: 0.5rem;
+      font-size: 1rem;
+      font-weight: bold;
+
+      &:hover {
+        box-shadow: 0 0 0.5rem 0.4rem rgba(0, 128, 128, 0.29);
+        background-color: green;
+        color: white;
+      }
+    }
+  }
+`;
 
 interface Param {
   id: number;
@@ -64,11 +114,11 @@ const ParamEditor: React.FC<Props> = ({ params, model }) => {
   };
 
   return (
-    <div>
+    <div className="MyWrap">
       {params &&
         params.map((param) => (
-          <div key={param.id}>
-            <label>{param.name}</label>
+          <div key={param.id} className="sting">
+            <label className="label">{param.name}</label>
             <input
               type="text"
               value={
@@ -80,16 +130,18 @@ const ParamEditor: React.FC<Props> = ({ params, model }) => {
             />
           </div>
         ))}
-      <button onClick={() => console.log(getModel())}>Получить модель</button>
+      <button className="btn" onClick={() => console.log(getModel())}>
+        Получить модель
+      </button>
     </div>
   );
 };
 
 const App: React.FC = () => {
   return (
-    <div>
+    <Wrap>
       <ParamEditor params={params} model={model} />
-    </div>
+    </Wrap>
   );
 };
 
